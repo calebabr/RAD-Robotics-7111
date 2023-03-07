29889e0f420e43e5615b87bc466355a025a6ff05
calebabr <86934442+calebabr@users.noreply.github.com>
2023-03-07 15:32:23 -0600

    3/7/2023
    added motor controller groups and arcadeDrive

--- WPI Examples/ChargedUp Robot/src/main/java/frc/robot/Robot.java
+++ WPI Examples/ChargedUp Robot/src/main/java/frc/robot/Robot.java
@@ -25,10 +25,11 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 
 
 /**
@@ -37,8 +38,6 @@ import edu.wpi.first.math.controller.PIDController;
  */
 public class Robot extends TimedRobot {
   private final PIDController gyroscope_pid = new PIDController(0.1, 0, 0);
-  private Joystick leftStick;
-  private Joystick rightStick;
   private XboxController m_xbox = new XboxController(2);
   private GenericEntry limelight_kP;
   private GenericEntry limelight_kI;
@@ -47,23 +46,35 @@ public class Robot extends TimedRobot {
   private float gyro_dead_range = 1;
 
   // Motors controlling driving
-  private final CANSparkMax motorfrontLeft = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-  private final CANSparkMax motorbackLeft = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-  private final CANSparkMax motorfrontRight = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-  private final CANSparkMax motorbackRight= new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax motorFrontLeft = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax motorBackLeft = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax motorFrontRight = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax motorBackRight= new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+  
+  // Motor Controller Groups
+  private final MotorControllerGroup left = new MotorControllerGroup(motorBackLeft, motorFrontLeft);
+  private final MotorControllerGroup right = new MotorControllerGroup(motorBackRight, motorFrontRight);
+
+  // RobotDrive
+  private DifferentialDrive robotDrive;
+  
   // Cone/Cube Grabber parts
   public static final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
   public static final Solenoid sol1 = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
   public static final Solenoid sol2 = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
+
   // limelight
   private final PhotonCamera limelight = new PhotonCamera("clockcam");
 
   private final PIDController limelight_pid = new PIDController(0.005, 0.00005, 0);
 
   // Joysticks
-  private double leftSpeed = 0;
-  private double rightSpeed = 0;
-  private double turnSpeed = 0;
+  private double ySpeed = 0;
+  private double rSpeed = 0;
+  private Joystick leftStick;
+  private Joystick rightStick;
+  private SlewRateLimiter rightJLimiter;
+  private SlewRateLimiter leftJLimiter;
 
   // Gyroscope
   private double gyro_set_drive_speed = 0;
@@ -90,11 +101,14 @@ public class Robot extends TimedRobot {
     limelight_pid.setSetpoint(0);
     limelight_pid.setTolerance(10);
 
-    motorfrontRight.setInverted(true);
-    motorbackRight.setInverted(true);
+    motorFrontRight.setInverted(true);
+    motorBackRight.setInverted(true);
     
     rightStick = new Joystick(0);
     leftStick = new Joystick(1);
+    leftJLimiter = new SlewRateLimiter(0.5); // needs to be tested
+    rightJLimiter = new SlewRateLimiter(0.5);
+    robotDrive = new DifferentialDrive(left, right);
   }
 
   @Override
@@ -116,8 +130,7 @@ public class Robot extends TimedRobot {
         // Calculate angular turn power
         // -1.0 required to ensure positive PID controller effort _increases_ yaw
         rotationSpeed = -limelight_pid.calculate(result.getBestTarget().getYaw(), 0);
-        leftSpeed = -rotationSpeed;
-        rightSpeed = rotationSpeed;
+        rSpeed = -rotationSpeed;
         
       } else {
         // If we have no targets, stay still.
@@ -134,19 +147,15 @@ public class Robot extends TimedRobot {
       else
       {
         gyro_set_drive_speed = gyroscope_pid.calculate(ahrsAngle, 0); // this is bad, needs to use an encoder !!!
-        leftSpeed = gyro_set_drive_speed;
-        rightSpeed = gyro_set_drive_speed;
+        ySpeed = gyro_set_drive_speed;
       }
     }
-    leftSpeed = gyro_set_drive_speed;
-    rightSpeed = gyro_set_drive_speed;
     // end gyroscope code
 
     // If we are not using gyroscope or limelight, then get joystick input for speed.
   } else {
-      leftSpeed = leftStick.getY();
-      rightSpeed = rightStick.getY();
-      turnSpeed = leftStick.getX();
+    ySpeed = leftJLimiter.calculate(leftStick.getY());
+    rSpeed = rightJLimiter.calculate(rightStick.getX());
     }
     // Start Solenoid code, for grabber.
     if (m_xbox.getXButton()) {
@@ -175,10 +184,8 @@ public class Robot extends TimedRobot {
     // end solenoid code.
 
     // Setting the desired speed to the motors.
-    motorfrontLeft.set(Math.pow(leftSpeed, 3));
-    motorbackLeft.set(Math.pow(leftSpeed, 3));
-    motorfrontRight.set(Math.pow(rightSpeed, 3));
-    motorbackRight.set(Math.pow(rightSpeed, 3));
+
+    robotDrive.arcadeDrive(ySpeed, rSpeed);
 
     SmartDashboard.putNumber("AHRS Angle (Gyroscope)", ahrsAngle);
 
