ce82e2284ccbb0cc5f6ca1317e4a0fee82414976
Seamaj <seamajesty1st@gmail.com>
2023-03-07 19:42:33 -0600

    More robot code
    Robot arm ready to go! Workin' on the gyro tho.

--- WPI Examples/ChargedUp Robot/src/main/java/frc/robot/Robot.java
+++ WPI Examples/ChargedUp Robot/src/main/java/frc/robot/Robot.java
@@ -4,15 +4,18 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 import edu.wpi.first.wpilibj.SPI.Port;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;        
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
 import javax.swing.plaf.RootPaneUI;
 
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -26,6 +29,7 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -43,7 +47,17 @@ public class Robot extends TimedRobot {
   private GenericEntry limelight_kI;
   private GenericEntry limelight_kD;
   private GenericEntry GyroMinMax;
-  private float gyro_dead_range = 1;
+  private float gyro_dead_range = 1; // tinker!
+  private static final int kEncoderPortA = 0;
+  private static final int kEncoderPortB = 1;
+  private final VictorSPX arm_motor = new VictorSPX(1);
+  private final VictorSPX arm_motor2 = new VictorSPX(2);
+  private double arm1speed;
+  private double arm2speed;
+  private double ArmmaxValue = 5.0; // tinker!
+  private double ArmminValue = 6.0; // tinker!
+  
+  private RelativeEncoder arm_encoder;
 
   // Motors controlling driving
   private final CANSparkMax motorFrontLeft = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -73,8 +87,10 @@ public class Robot extends TimedRobot {
   private double rSpeed = 0;
   private Joystick leftStick;
   private Joystick rightStick;
-  private SlewRateLimiter rightJLimiter;
+  private SlewRateLimiter rightJLimiter; // tinker?
   private SlewRateLimiter leftJLimiter;
+  double rotationSpeed;
+  private float limelightdistancetotarget; // tinker deadzone
 
   // Gyroscope
   private double gyro_set_drive_speed = 0;
@@ -85,10 +101,11 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     // Setting up variables on the smart dashboard so we can change them while running the code.
-    limelight_kP = Shuffleboard.getTab("SmartDashboard").add("kP", 0.01).withWidget("Text View").getEntry();
-    limelight_kI = Shuffleboard.getTab("SmartDashboard").add("kI", 0.00005).withWidget("Text View").getEntry();
-    limelight_kD = Shuffleboard.getTab("SmartDashboard").add("kD", 0).withWidget("Text View").getEntry();
-    GyroMinMax = Shuffleboard.getTab("SmartDashboard").add("Gyroscope_deadzone", 1).withWidget("Text View").getEntry();
+    limelight_kP = Shuffleboard.getTab("SmartDashboard").add("kP", 0.01).withWidget("Text View").getEntry(); // tinker with this
+    limelight_kI = Shuffleboard.getTab("SmartDashboard").add("kI", 0.00005).withWidget("Text View").getEntry(); // tinker with this
+    limelight_kD = Shuffleboard.getTab("SmartDashboard").add("kD", 0).withWidget("Text View").getEntry(); // don't need this probably
+    GyroMinMax = Shuffleboard.getTab("SmartDashboard").add("Gyroscope_deadzone", 1).withWidget("Text View").getEntry(); // tinker!
+    arm_encoder.setPosition(0);
   }
   @Override
   public void robotInit() {
@@ -99,7 +116,7 @@ public class Robot extends TimedRobot {
     ahrs_gyroscope.reset();
 
     limelight_pid.setSetpoint(0);
-    limelight_pid.setTolerance(10);
+    limelight_pid.setTolerance(10); // Important! Tinker?
 
     motorFrontRight.setInverted(true);
     motorBackRight.setInverted(true);
@@ -110,7 +127,37 @@ public class Robot extends TimedRobot {
     rightJLimiter = new SlewRateLimiter(0.5);
     robotDrive = new DifferentialDrive(left, right);
   }
+  @Override
+  public void autonomousInit() {}
 
+  @Override
+  public void autonomousPeriodic() {
+    limelight_pid.setP(limelight_kP.getDouble(0.005)); //change these if you change the main limelight value.
+    limelight_pid.setI(limelight_kI.getDouble(0.0005));
+    limelight_pid.setD(limelight_kD.getDouble(0));
+    limelight_pid.setD(GyroMinMax.getDouble(1));
+    var result = limelight.getLatestResult();
+    if (result.hasTargets()) {
+      // Calculate angular turn power
+      // -1.0 required to ensure positive PID controller effort _increases_ yaw
+      rotationSpeed = -limelight_pid.calculate(result.getBestTarget().getYaw(), 0);
+      rSpeed = -rotationSpeed;
+      limelightdistancetotarget = result.targets.size();
+      if (limelightdistancetotarget < 1); // Tune how far we want this number, tinker with this
+        motorFrontLeft.set(0);
+        motorFrontRight.set(0);
+        motorBackLeft.set(0);
+        motorBackRight.set(0);
+    } else {
+      // If we have no targets, rotate.
+      motorFrontLeft.set(0.15);
+      motorFrontRight.set(0.15);
+      motorBackLeft.set(-0.15);
+      motorBackRight.set(-0.15);
+  }
+  // end of code for the limelight
+
+  }
   @Override
   public void teleopPeriodic() {
     // Setting up gyroscope angles
@@ -118,7 +165,6 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("AHRS Angle (Gyroscope)", ahrsAngle);
 
     // Start of code for the limelight
-    double rotationSpeed;
     // Code to set variables for the limelight_pid, so we can tinker with them while it is running.
     limelight_pid.setP(limelight_kP.getDouble(0.005));
     limelight_pid.setI(limelight_kI.getDouble(0.0005));
@@ -131,6 +177,8 @@ public class Robot extends TimedRobot {
         // -1.0 required to ensure positive PID controller effort _increases_ yaw
         rotationSpeed = -limelight_pid.calculate(result.getBestTarget().getYaw(), 0);
         rSpeed = -rotationSpeed;
+        ySpeed = 0.3;
+        robotDrive.arcadeDrive(ySpeed, rSpeed);
         
       } else {
         // If we have no targets, stay still.
@@ -140,13 +188,13 @@ public class Robot extends TimedRobot {
       // Start code for gyroscope
     if (m_xbox.getBButton()){
       // if our gyroscope is in a certain range, we don't move.
-      if (ahrsAngle < gyro_dead_range && ahrsAngle > -gyro_dead_range){
+      if (ahrsAngle < gyro_dead_range && ahrsAngle > -gyro_dead_range){ // tinker this deadzone
         gyro_set_drive_speed = 0;
       }
         // otherwise, we calculate where we need to move and set our left and right speed. 
       else
       {
-        gyro_set_drive_speed = gyroscope_pid.calculate(ahrsAngle, 0); // this is bad, needs to use an encoder !!!
+        gyro_set_drive_speed = gyroscope_pid.calculate(ahrsAngle, 0); // this is bad, simon is updating.
         ySpeed = gyro_set_drive_speed;
       }
     }
@@ -183,11 +231,26 @@ public class Robot extends TimedRobot {
     }
     // end solenoid code.
 
+    // Start arm code
+    arm1speed = m_xbox.getLeftY();
+    arm2speed = m_xbox.getRightY();
+    if (arm_encoder.getPosition() <= ArmmaxValue && arm_encoder.getPosition() >= ArmminValue) // tinker with this encoder!
+    {
+      arm_motor.set(VictorSPXControlMode.PercentOutput, arm1speed);
+    }
+    arm_motor2.set(VictorSPXControlMode.PercentOutput, arm2speed);
+
+    // End arm code
+    
+  
     // Setting the desired speed to the motors.
 
     robotDrive.arcadeDrive(ySpeed, rSpeed);
 
+    //setting variables to smartdashboard.
     SmartDashboard.putNumber("AHRS Angle (Gyroscope)", ahrsAngle);
+
+    SmartDashboard.putNumber("Limelight Distance to target", limelightdistancetotarget);
 
     }
     public double remap_range(double val, double old_min, double old_max, double new_min, double new_max){ // Basically just math to convert a value from an old range to 
