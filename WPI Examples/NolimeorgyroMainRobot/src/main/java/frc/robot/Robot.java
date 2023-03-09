545394b6e4f9e7b5b59e57b81fdf7a70521a815a
Seamaj <seamajesty1st@gmail.com>
2023-03-09 15:48:10 -0600

    Edits!
    Yay!

--- WPI Examples/NolimeorgyroMainRobot/src/main/java/frc/robot/Robot.java
+++ WPI Examples/NolimeorgyroMainRobot/src/main/java/frc/robot/Robot.java
@@ -16,10 +16,10 @@ import javax.swing.plaf.RootPaneUI;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import com.kauailabs.navx.frc.AHRS;
+//import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.photonvision.PhotonCamera;
+//import org.photonvision.PhotonCamera;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -41,13 +41,7 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private final PIDController gyroscope_pid = new PIDController(0.1, 0, 0);
   private XboxController m_xbox = new XboxController(2);
-  private GenericEntry limelight_kP;
-  private GenericEntry limelight_kI;
-  private GenericEntry limelight_kD;
-  private GenericEntry GyroMinMax;
-  private float gyro_dead_range = 1; // tinker!
   private static final int kEncoderPortA = 0;
   private static final int kEncoderPortB = 1;
   private final VictorSPX arm_motor = new VictorSPX(1);
@@ -59,30 +53,27 @@ public class Robot extends TimedRobot {
   
   private RelativeEncoder arm_encoder;
 
-  // Motors controlling driving
+  // Motors controlling 
   private final CANSparkMax motorFrontLeft = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
   private final CANSparkMax motorBackLeft = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
   private final CANSparkMax motorFrontRight = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
   private final CANSparkMax motorBackRight= new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
   
-  // Motor Controller Groups
+  // Motor Controller 
   private final MotorControllerGroup left = new MotorControllerGroup(motorBackLeft, motorFrontLeft);
   private final MotorControllerGroup right = new MotorControllerGroup(motorBackRight, motorFrontRight);
 
-  // RobotDrive
+  // 
   private DifferentialDrive robotDrive;
   
-  // Cone/Cube Grabber parts
+  // Cone/Cube Grabber 
   public static final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
   public static final Solenoid sol1 = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
   public static final Solenoid sol2 = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
 
-  // limelight
-  private final PhotonCamera limelight = new PhotonCamera("clockcam");
+  
 
-  private final PIDController limelight_pid = new PIDController(0.005, 0.00005, 0);
-
-  // Joysticks
+  // 
   private double ySpeed = 0;
   private double rSpeed = 0;
   private Joystick leftStick;
@@ -90,21 +81,8 @@ public class Robot extends TimedRobot {
   private SlewRateLimiter rightJLimiter; // tinker?
   private SlewRateLimiter leftJLimiter;
   double rotationSpeed;
-  private float limelightdistancetotarget; // tinker deadzone
-
-  // Gyroscope
-  private double gyro_set_drive_speed = 0;
-  
-  private AHRS ahrs_gyroscope;
-
-  private float ahrsAngle;
   @Override
   public void teleopInit() {
-    // Setting up variables on the smart dashboard so we can change them while running the code.
-    limelight_kP = Shuffleboard.getTab("SmartDashboard").add("kP", 0.01).withWidget("Text View").getEntry(); // tinker with this
-    limelight_kI = Shuffleboard.getTab("SmartDashboard").add("kI", 0.00005).withWidget("Text View").getEntry(); // tinker with this
-    limelight_kD = Shuffleboard.getTab("SmartDashboard").add("kD", 0).withWidget("Text View").getEntry(); // don't need this probably
-    GyroMinMax = Shuffleboard.getTab("SmartDashboard").add("Gyroscope_deadzone", 1).withWidget("Text View").getEntry(); // tinker!
     arm_encoder.setPosition(0);
   }
   @Override
@@ -112,11 +90,7 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    ahrs_gyroscope = new AHRS(Port.kMXP);
-    ahrs_gyroscope.reset();
-
-    limelight_pid.setSetpoint(0);
-    limelight_pid.setTolerance(10); // Important! Tinker?
+    
 
     motorFrontRight.setInverted(true);
     motorBackRight.setInverted(true);
@@ -127,84 +101,11 @@ public class Robot extends TimedRobot {
     rightJLimiter = new SlewRateLimiter(0.5);
     robotDrive = new DifferentialDrive(left, right);
   }
-  @Override
-  public void autonomousInit() {}
 
-  @Override
-  public void autonomousPeriodic() {
-    limelight_pid.setP(limelight_kP.getDouble(0.005)); //change these if you change the main limelight value.
-    limelight_pid.setI(limelight_kI.getDouble(0.0005));
-    limelight_pid.setD(limelight_kD.getDouble(0));
-    limelight_pid.setD(GyroMinMax.getDouble(1));
-    var result = limelight.getLatestResult();
-    if (result.hasTargets()) {
-      // Calculate angular turn power
-      // -1.0 required to ensure positive PID controller effort _increases_ yaw
-      rotationSpeed = -limelight_pid.calculate(result.getBestTarget().getYaw(), 0);
-      rSpeed = -rotationSpeed;
-      limelightdistancetotarget = result.targets.size();
-      if (limelightdistancetotarget < 1); // Tune how far we want this number, tinker with this
-        motorFrontLeft.set(0);
-        motorFrontRight.set(0);
-        motorBackLeft.set(0);
-        motorBackRight.set(0);
-    } else {
-      // If we have no targets, rotate.
-      motorFrontLeft.set(0.15);
-      motorFrontRight.set(0.15);
-      motorBackLeft.set(-0.15);
-      motorBackRight.set(-0.15);
-  }
-  // end of code for the limelight
-
-  }
   @Override
   public void teleopPeriodic() {
-    // Setting up gyroscope angles
-    ahrsAngle = ahrs_gyroscope.getPitch();
-    SmartDashboard.putNumber("AHRS Angle (Gyroscope)", ahrsAngle);
-
-    // Start of code for the limelight
-    // Code to set variables for the limelight_pid, so we can tinker with them while it is running.
-    limelight_pid.setP(limelight_kP.getDouble(0.005));
-    limelight_pid.setI(limelight_kI.getDouble(0.0005));
-    limelight_pid.setD(limelight_kD.getDouble(0));
-    limelight_pid.setD(GyroMinMax.getDouble(1));
-    if (m_xbox.getAButton()){
-      var result = limelight.getLatestResult();
-      if (result.hasTargets()) {
-        // Calculate angular turn power
-        // -1.0 required to ensure positive PID controller effort _increases_ yaw
-        rotationSpeed = -limelight_pid.calculate(result.getBestTarget().getYaw(), 0);
-        rSpeed = -rotationSpeed;
-        ySpeed = 0.3;
-        robotDrive.arcadeDrive(ySpeed, rSpeed);
-        
-      } else {
-        // If we have no targets, stay still.
-        rotationSpeed = 0;
-    }
-    // end of code for the limelight
-      // Start code for gyroscope
-    if (m_xbox.getBButton()){
-      // if our gyroscope is in a certain range, we don't move.
-      if (ahrsAngle < gyro_dead_range && ahrsAngle > -gyro_dead_range){ // tinker this deadzone
-        gyro_set_drive_speed = 0;
-      }
-        // otherwise, we calculate where we need to move and set our left and right speed. 
-      else
-      {
-        gyro_set_drive_speed = gyroscope_pid.calculate(ahrsAngle, 0); // this is bad, simon is updating.
-        ySpeed = gyro_set_drive_speed;
-      }
-    }
-    // end gyroscope code
-
-    // If we are not using gyroscope or limelight, then get joystick input for speed.
-  } else {
     ySpeed = leftJLimiter.calculate(leftStick.getY());
     rSpeed = rightJLimiter.calculate(rightStick.getX());
-    }
     // Start Solenoid code, for grabber.
     if (m_xbox.getYButton()) {
       // this is cone grab mode, press again to end.
@@ -235,11 +136,6 @@ public class Robot extends TimedRobot {
     // Setting the desired speed to the motors.
 
     robotDrive.arcadeDrive(ySpeed, rSpeed);
-
-    //setting variables to smartdashboard.
-    SmartDashboard.putNumber("AHRS Angle (Gyroscope)", ahrsAngle);
-
-    SmartDashboard.putNumber("Limelight Distance to target", limelightdistancetotarget);
 
     }
     public double remap_range(double val, double old_min, double old_max, double new_min, double new_max){ // Basically just math to convert a value from an old range to 
