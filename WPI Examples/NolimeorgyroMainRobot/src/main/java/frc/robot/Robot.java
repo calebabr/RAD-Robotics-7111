a26f39b673a083550784651cd472df4be2b02f7c
calebabr <86934442+calebabr@users.noreply.github.com>
2023-03-13 11:46:04 -0500

    Update Robot.java

--- WPI Examples/NolimeorgyroMainRobot/src/main/java/frc/robot/Robot.java
+++ WPI Examples/NolimeorgyroMainRobot/src/main/java/frc/robot/Robot.java
@@ -92,7 +92,7 @@ public class Robot extends TimedRobot {
   // Cone/Cube Grabber 
   public static final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
   public static final DoubleSolenoid sol1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 0);
-  public static final DoubleSolenoid sol2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+  // public static final DoubleSolenoid sol2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
 
 
   // 
@@ -186,11 +186,11 @@ public class Robot extends TimedRobot {
       // this is cone grab mode, press again to end.
       // if you press when a solenoid is already active, it resets it.
       sol1.set(DoubleSolenoid.Value.kForward);
-      sol2.set(DoubleSolenoid.Value.kForward);
+      // sol2.set(DoubleSolenoid.Value.kForward);
     }
     else if (m_xbox.getXButtonPressed()){
       sol1.set(DoubleSolenoid.Value.kReverse);
-      sol2.set(DoubleSolenoid.Value.kReverse);
+      // sol2.set(DoubleSolenoid.Value.kReverse);
     }
 
     // Claw Motors
