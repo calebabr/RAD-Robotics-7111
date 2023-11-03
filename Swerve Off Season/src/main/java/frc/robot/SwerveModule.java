5cdd6caeae9a66ad0fab78f80b29676c6b9f8841
calebabr <86934442+calebabr@users.noreply.github.com>
2023-11-03 18:56:26 -0500

    fgh

--- Swerve Off Season/src/main/java/frc/robot/SwerveModule.java
+++ Swerve Off Season/src/main/java/frc/robot/SwerveModule.java
@@ -42,7 +42,7 @@ public class SwerveModule {
   // Gains are for example purposes only - must be determined for your own robot!
   private final ProfiledPIDController m_turningPIDController =
       new ProfiledPIDController(
-          0.1,
+          0,
           0,
           0,
           new TrapezoidProfile.Constraints(
